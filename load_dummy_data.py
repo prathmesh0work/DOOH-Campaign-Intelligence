096e4e73c6213112ed75d539bc869afbd8d65179
Prathmesh Ingole <prathmesh0work@gmail.com>
2026-08-12 12:53:17 +0530

    Fix formatting issues in load_dummy_data.py

--- load_dummy_data.py
+++ load_dummy_data.py
@@ -8,15 +8,13 @@ def load_csv_to_database(csv_path):
     raw_rows = table.to_dict('records')
 
     cleaning_result = clean_data(raw_rows)
-    clean_rows =cleaning_result["rows"]
+    clean_rows = cleaning_result["rows"]
     log = cleaning_result["log"]
 
-    print(f"cleaned {log['final_rows']} rows out of{log['total_rows']} original rows.")
-    
+    print(f"cleaned {log['final_rows']} rows out of {log['total_rows']} original rows.")
+
     row_inserted = insert_campaigns(clean_rows)
-    
     print(f"Inserted {row_inserted} rows into the campaigns table")
 
 if __name__ == "__main__":
     load_csv_to_database("dummy_dataset.csv")
-
